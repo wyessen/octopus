19ff6c2dc745015cca3f1a81e7d571b5ad91f4d4
CrankyOctopus <CrankyOctopus>
2026-04-22 01:46:23 -0700

    Better makefile handling

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -36,3 +36,17 @@ autocmd("VimLeavePre", {
     end,
 })
 
+-- We want Makefile to be picked up every time one exists,
+-- regardless of the file type.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "sh",
+  callback = function()
+    -- Only use bash -n if a Makefile DOES NOT exist
+    if vim.fn.filereadable("Makefile") == 0 then
+      vim.opt_local.makeprg = "bash -n"
+    else
+      vim.opt_local.makeprg = "make"
+    end
+  end,
+})
+
