25507819dab8d9e006a7bc14a4a0dda3a667895d
CrankyOctopus <CrankyOctopus>
2026-04-22 00:41:39 -0700

    Smarter file search handling

--- lua/ui.lua
+++ lua/ui.lua
@@ -1,22 +1,23 @@
 require('telescope').setup({
-  defaults = {
-    -- This affects live_grep and other search tools
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      "--hidden",          -- Search hidden files
+    defaults = {
+        -- This affects live_grep and other search tools
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",     -- Include hidden files
+            "--glob", "!.git/*" -- Exclude .git directory
+        },
     },
-  },
-  pickers = {
-    find_files = {
-      hidden = true, 
-      find_command = { "fd", "--type", "f", "--hidden", "--exclude", ".git" },
+    pickers = {
+        find_files = {
+            hidden = true, 
+            find_command = { "fd", "--type", "f", "--hidden", "--exclude", ".git" },
+        },
     },
-  },
 })
 
